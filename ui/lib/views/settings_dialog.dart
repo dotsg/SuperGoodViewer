e6deb9dbba58459476d0eca17fa4c77a94251ccf
Weng Wei <wuvist@gmail.com>
2026-09-14 00:44:18 +0800

    chore(release): release v1.0.4

--- ui/lib/views/settings_dialog.dart
+++ ui/lib/views/settings_dialog.dart
@@ -32,7 +32,7 @@ void showSettingsDialog(
 }
 
 class SettingsDialog extends StatefulWidget {
-  static const String appVersion = '1.0.3';
+  static const String appVersion = '1.0.4';
 
   final ReaderController controller;
   final SettingsTab initialTab;
